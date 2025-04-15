72ae1ef7a14bd665e3e016835f9581fc9ab1c420
Your Name <you@example.com>
2025-04-15 18:12:39 +0200

    receptek megjelenítve

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import Recipe from './components/Recipe'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const BASE_URL = "http://127.0.0.1:8000"
+
+const App = () => {
+
+  const [receptek, setReceptek] = useState([])
+
+  useEffect(()=>{
+  fetch(BASE_URL + "/recipes/")
+  .then((valasz)=>{return valasz.json()})
+  .then((receptek)=>{setReceptek(receptek)})
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {receptek.map(recept => <Recipe {...recept}/>)}
+    </div>
   )
 }
 
